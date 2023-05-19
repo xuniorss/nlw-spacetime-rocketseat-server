b3868cffcbb3b7a236eab1951b40dc87df46fb95
xuniorss <fortunatojunior2020@gmail.com>
2023-05-18 22:54:12 -0300

    quarto dia

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,29 @@
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import fastify from 'fastify'
+import { resolve } from 'node:path'
 
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 import 'dotenv/config'
 
 const app = fastify()
 
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+   root: resolve(__dirname, '../uploads'),
+   prefix: '/uploads',
+})
+
 app.register(cors, { origin: true })
 app.register(jwt, { secret: process.env.SECRET_KEY as string })
 
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 app.register(authRoutes)
 
 app.listen({
